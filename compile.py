3b59adf415e5ae5a5d0113b9d3a4b67014ed7348
Andrej Karpathy <andrej.karpathy@gmail.com>
2023-05-24 22:53:40 -0700

    reshuffle things a bit

--- compile.py
+++ compile.py
@@ -2,33 +2,47 @@
 # so e.g. we don't import level1.txt, but level1_compiled.txt, the output of this script
 # we also do some basic error checking, like making sure there's only one = delimiter
 
-# input output
-lines = open('level1.txt', 'r').readlines()
-output = open('level1_compiled.txt', 'w')
+import os
 
-# process all the lines
-ncards = 0
-for line in lines:
+# output directory
+in_dir = 'cards'
+out_dir = 'compiled'
+os.makedirs(out_dir, exist_ok=True)
 
-    # next line, strip any whitespace on both ends
-    sline = line.strip()
+# get all the sections from cards/
+sections = os.listdir(in_dir)
 
-    # skip empty lines
-    if len(sline) == 0:
-        continue
+for section in sections:
 
-    # preserve comments
-    if sline[0] == '#':
-        output.write(line)
-        continue
+    in_path = os.path.join(in_dir, section)
+    out_path = os.path.join(out_dir, section)
 
-    # find either the = delimiter, and add both the front and back of the card
-    parts = [p.strip() for p in sline.split('=')]
-    assert len(parts) == 2, 'Invalid line: ' + line
+    lines = open(in_path, 'r').readlines()
+    output = open(out_path, 'w')
 
-    # write to output both the normal and reversed order
-    output.write(parts[0] + ';' + parts[1] + '\n') # normal order
-    output.write(parts[1] + ';' + parts[0] + '\n') # reversed order
-    ncards += 2
+    # process all the lines
+    ncards = 0
+    for line in lines:
 
-print(ncards, 'cards compiled')
+        # next line, strip any whitespace on both ends
+        sline = line.strip()
+
+        # skip empty lines
+        if len(sline) == 0:
+            continue
+
+        # preserve comments
+        if sline[0] == '#':
+            output.write(line)
+            continue
+
+        # find either the = delimiter, and add both the front and back of the card
+        parts = [p.strip() for p in sline.split('=')]
+        assert len(parts) == 2, 'Invalid line: ' + line
+
+        # write to output both the normal and reversed order
+        output.write(parts[0] + ';' + parts[1] + '\n') # normal order
+        output.write(parts[1] + ';' + parts[0] + '\n') # reversed order
+        ncards += 2
+
+    print(section, ncards, 'cards compiled')
